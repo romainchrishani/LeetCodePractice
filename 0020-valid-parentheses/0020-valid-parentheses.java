c8620b8d32f862bc6ab3daccf301bdbecc41c808
Romain Perera <perera.romainc@gmail.com>
2025-09-29 09:53:30 +0530

    Time: 2 ms (96.87%), Space: 41.9 MB (47.08%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean isValid(String s) {
+        char[] characters = s.toCharArray();
         Stack<Character> stack = new Stack<>();
 
-        for(char c : s.toCharArray()){
-            if( c == '('  || c == '{' || c == '['){
+        for(char c : characters){
+            if(c == '(' || c == '[' || c == '{'){
                 stack.push(c);
             }
             else{
@@ -12,15 +13,14 @@ class Solution {
                 }
                 else{
                     char top = stack.pop();
-                    if( c == ')' && top != '(' || 
-                        c == '}' && top != '{' ||
-                        c == ']' && top != '[' )
-                        {
+                    if((c == ')' && top != '(') || (c == ']' && top != '[') || (c == '}' && top != '{')){
                         return false;
                     }
                 }
             }
         }
         return stack.isEmpty();
+
+
     }
 }
