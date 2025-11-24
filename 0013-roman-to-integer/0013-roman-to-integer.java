e9c7dfcd1d5c5cea0651a95aac8108a395927779
Romain Perera <perera.romainc@gmail.com>
2025-11-24 13:47:06 +0530

    Time: 4 ms (69.34%), Space: 46.3 MB (27.49%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,33 +1,31 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character,Integer> romanToInteger = new HashMap<>();
+        HashMap<Character,Integer> RomanToInteger = new HashMap<>();
 
-        romanToInteger.put('I',1);
-        romanToInteger.put('V',5);
-        romanToInteger.put('X',10);
-        romanToInteger.put('L',50);
-        romanToInteger.put('C',100);
-        romanToInteger.put('D',500);
-        romanToInteger.put('M',1000);
+        RomanToInteger.put('I',1);
+        RomanToInteger.put('V',5);
+        RomanToInteger.put('X',10);
+        RomanToInteger.put('L',50);
+        RomanToInteger.put('C',100);
+        RomanToInteger.put('D',500);
+        RomanToInteger.put('M',1000);
 
-        
-
-        int number = 0;
         int previous = 0;
+        int number = 0;
 
-        for(int i = s.length()-1;i>=0;i--){
-            int current = romanToInteger.get(s.charAt(i));
+        for(int i=s.length()-1;i>=0;i--){
+
+            int Current = RomanToInteger.get(s.charAt(i));
             
-            if(current>=previous){
-                number=number+current;
+            
+            if(Current >= previous){
+                number = number + Current; 
             }
             else{
-                number=number-current;
+                number = number - Current;
             }
-            previous=current;
-            
+            previous = Current;
         }
         return number;
-
     }
 }
